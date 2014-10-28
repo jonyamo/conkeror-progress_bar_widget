8cc4a6320233e14bb8497b72d002fbff4f7c2ee9
Jon Yamokoski <code@jonyamo.us>
2014-10-28 14:01:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Conkeror Progress Bar Widget
+
+A progress bar widget for [Conkeror](http://conkeror.org/)
+
+## Introduction
+
+When enabled a progress bar widget will be added to Conkeror's mode-line.  It
+will display the current buffer's loading progress as well as the amount of
+time elapsed, like so:
+
+    [#####     ] 0.3s
+
+## Installation
+
+Simply copy `progress_bar.js` to your `.conkerorrc` directory.
+
+Alternatively, add `require("/path/to/progress_bar.js")` to your `.conkerorrc` file.
---
+++ progress_bar.js
@@ -0,0 +1,75 @@
+/* progress_bar.js -- A progress bar widget for Conkeror
+
+ Copyright (c) 2014, Jon Yamokoski <code@jonyamo.us>
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+function progress_bar_widget (window) {
+    text_widget.call(this, window);
+    this.add_hook("content_buffer_started_loading_hook",
+                  method_caller(this, this.init));
+    this.add_hook("content_buffer_progress_change_hook",
+                  method_caller(this, this.update));
+    this.add_hook("content_buffer_finished_loading_hook",
+                  method_caller(this, this.destroy));
+}
+
+progress_bar_widget.prototype = {
+    constructor: progress_bar_widget,
+    __proto__: text_widget.prototype,
+    class_name: "progress-bar-widget",
+
+    init_time: null,
+    pad_len: 10,
+
+    init: function () {
+        this.init_time = new Date;
+
+        var percent_str = "#" + Array(this.pad_len).join(" ");
+        var elapsed_str = "0.0s";
+
+        this.view.text = "[{0}] {1}".replace("{0}", percent_str).replace("{1}", elapsed_str);
+    },
+
+    update: function (buffer, request, curSelf, maxSelf, curTotal, maxTotal) {
+        if (typeof buffer == 'undefined')
+            return;
+
+        if (maxTotal < 1)
+            return;
+
+        // https://github.com/tj/node-progress
+        var ratio = curTotal / maxTotal;
+        ratio = Math.min(Math.max(ratio, 0), 1);
+
+        var percent = ratio * 100;
+        var elapsed = new Date - this.init_time;
+        elapsed = isNaN(elapsed) ? "0.0" : (elapsed / 1000).toFixed(1);
+
+        var percent_str = "#".repeat(percent.toFixed(0) / 10);
+        if (percent_str < this.pad_len)
+            percent_str = percent_str + Array(this.pad_len + 1 - percent_str.length).join(" ");
+        var elapsed_str = elapsed + "s";
+
+        this.view.text = "[{0}] {1}".replace("{0}", percent_str).replace("{1}", elapsed_str);
+    },
+
+    destroy: function () {
+        this.init_time = null;
+        this.view.text = "";
+    }
+};
+
+add_hook("mode_line_hook", mode_line_adder(progress_bar_widget));
